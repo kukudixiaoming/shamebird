119e0acb7f19291386f804339c91ad42e12be392
kukudixiaoming <kukudixiaoming@gmail.com>
2016-02-28 15:44:18 +0800

    新加了纸条fm的视图

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  def post_music(id)
+    href = "http://music.163.com/outchain/player?type=2&id=#{id}&auto=0&height=66"
+    html = "<iframe frameborder='no' border='0' marginwidth='0' marginheight='0' width=330 height=86 src=""#{href}""></iframe>"
+    raw html
+  end
 end
