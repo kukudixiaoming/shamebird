6aff4b9d1dcf14ad1513ccc29f518001c58b96ef
kukudixiaoming <kukudixiaoming@gmail.com>
2016-03-02 16:10:05 +0800

    完成了comment的create，还存在两个bug：会多出一条空评论和时间的显示有问题

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :category
+  has_many :comments
   validates :post_title, presence: true, length: {maximum: 70}
 
   validates :post_text, presence: true
